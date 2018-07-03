92c631d079263fe4f5ac3e76b0155f4b0a67ecff
Alexey Stepanov <penguinolog@gmail.com>
2018-07-03 15:09:09 +0200

    Bump to 1.0.4
    * Enforce py37 wheels, manual py27 wheel
    * reduce sdist size
    * remove todo: use github issues
    
    Signed-off-by: Alexey Stepanov <penguinolog@gmail.com>

--- advanced_descriptors/__init__.py
+++ advanced_descriptors/__init__.py
@@ -21,7 +21,7 @@ __all__ = (
     'AdvancedProperty',
 )
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = "Alexey Stepanov"
 __author_email__ = 'penguinolog@gmail.com'
 __maintainers__ = {
